7146054ae5ecc157113343436195badb6ec42e8d
rajdeepdinda <55017947+rajdeepdinda@users.noreply.github.com>
2019-09-07 21:49:41 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -6,4 +6,4 @@ cd source-code
 mvn clean package
 cd ..
 
-cp source-code/target/hello-world-mule-0.0.1-SNAPSHOT.jar  build-output/.
+cp source-code/target/mule-hello-world-1.0.0-SNAPSHOT.jar  build-output/.
